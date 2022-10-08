f46a599f6342410e3cb7f6e07ceab822b1d78b92
Harshil <hr830930@dal.ca>
2022-10-08 13:56:34 -0300

    test message in gitlab

--- src/test/java/ca/dal/grp16/tripmate/TripmateApplicationTests.java
+++ src/test/java/ca/dal/grp16/tripmate/TripmateApplicationTests.java
@@ -3,11 +3,17 @@ package ca.dal.grp16.tripmate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class TripmateApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    void test() {
+        assertTrue(true);
+    }
 }
