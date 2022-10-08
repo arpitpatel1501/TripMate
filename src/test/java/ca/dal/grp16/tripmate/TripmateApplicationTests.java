5b5b303da90f650bdd6efb43ad29ba6d86931d19
Harshil <hr830930@dal.ca>
2022-10-08 12:53:09 -0300

    changed java version to 1.8

--- src/test/java/ca/dal/grp16/tripmate/TripmateApplicationTests.java
+++ src/test/java/ca/dal/grp16/tripmate/TripmateApplicationTests.java
@@ -3,18 +3,11 @@ package ca.dal.grp16.tripmate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.springframework.test.util.AssertionErrors.assertTrue;
-
 @SpringBootTest
 class TripmateApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void testForPipeline(){
-		assertTrue("This will succeed.", true);
 	}
 
 }
