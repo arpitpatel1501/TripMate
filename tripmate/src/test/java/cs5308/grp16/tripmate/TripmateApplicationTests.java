b8769588ca2774d06e5c5f5447704d273a146adc
Library User <lib-user@libmac25.local>
2022-10-25 12:30:36 -0300

    Added Web

---
+++ tripmate/src/test/java/cs5308/grp16/tripmate/TripmateApplicationTests.java
@@ -0,0 +1,13 @@
+package cs5308.grp16.tripmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
