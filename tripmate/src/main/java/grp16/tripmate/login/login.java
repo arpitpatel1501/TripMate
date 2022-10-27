72541d4268f7cac266954db4e2d316571b9dc5a8
Harshil <lib-user@TEE96.WPA.Dal.Ca>
2022-10-27 09:22:41 -0300

    testing heroku

--- tripmate/src/main/java/grp16/tripmate/login/login.java
+++ tripmate/src/main/java/grp16/tripmate/login/login.java
@@ -12,10 +12,10 @@ public class login {
         return "greeting";
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "index";
-    }
+//    @RequestMapping(value = "/", method = RequestMethod.GET)
+//    public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "index";
+//    }
 
 }
