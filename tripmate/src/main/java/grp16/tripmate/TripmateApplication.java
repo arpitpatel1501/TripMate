d839d7525cbb32c1adc3925823bc970d9d7cf26e
Harshil <hr830930@dal.ca>
2022-10-26 21:39:03 -0300

    test greeting

--- tripmate/src/main/java/grp16/tripmate/TripmateApplication.java
+++ tripmate/src/main/java/grp16/tripmate/TripmateApplication.java
@@ -2,8 +2,10 @@ package grp16.tripmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "grp16.tripmate.login")
 public class TripmateApplication {
 
 	public static void main(String[] args) {
