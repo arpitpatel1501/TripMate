89c916e3ead4ce47db6a9a9079ed560bdb080ca1
Harshil <lib-user@TEE96.WPA.Dal.Ca>
2022-10-27 09:22:41 -0300

    testing heroku

--- tripmate/src/main/java/grp16/tripmate/TripmateApplication.java
+++ tripmate/src/main/java/grp16/tripmate/TripmateApplication.java
@@ -3,10 +3,20 @@ package grp16.tripmate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class TripmateApplication {
-
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "index";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TripmateApplication.class, args);
 	}
