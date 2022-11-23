125fe1e4eecac3cf98f1dda00de4e248d7d16636
amanjot <am350894@dal.ca>
2022-11-23 07:14:41 -0400

    Merging Conflicts

--- tripmate/src/main/java/grp16/tripmate/user/persistance/SQLPersistance.java
+++ tripmate/src/main/java/grp16/tripmate/user/persistance/SQLPersistance.java
@@ -2,19 +2,19 @@ package grp16.tripmate.user.persistance;
 
 import grp16.tripmate.db.DatabaseConnectionException;
 import grp16.tripmate.db.connection.DatabaseConnection;
-import grp16.tripmate.db.connection.DatabaseConnectionDAO;
+import grp16.tripmate.db.connection.IDatabaseConnection;
 import grp16.tripmate.user.model.User;
 
 import java.sql.Connection;
 
 public class SQLPersistance implements IUserPersistance{
-    DatabaseConnectionDAO connectionDAO = new DatabaseConnection();
+    DatabaseConnection databaseConnection = new DatabaseConnection();
     Connection connection;
 
     @Override
     public User getUser() throws Exception {
-        connection = connectionDAO.getDatabaseConnection();
-        connectionDAO.clearDatabaseConnection();
+        connection = databaseConnection.getDatabaseConnection();
+        connection.close();
         return null;
     }
 }
