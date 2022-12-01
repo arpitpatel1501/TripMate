754d33c127c7a60b8fa7989d47d42fabd4c0685a
Arpitkumar Patel <ar260217@dal.ca>
2022-12-01 09:37:11 -0400

    Interface for Notification service.

---
+++ tripmate/src/main/java/grp16/tripmate/notification/INotification.java
@@ -0,0 +1,7 @@
+package grp16.tripmate.notification;
+
+public interface INotification {
+    public void verification(String userEmail);
+    public void requestNotification();
+    public void responseNotification();
+}
