96c6f0b7ef1c5d16f9f7e4b282c6584e4362e31c
arpitkumar <ar260217@dal.ca>
2022-12-07 12:02:03 -0400

    INotification and IVerification for user verification and notification service.

--- tripmate/src/main/java/grp16/tripmate/notification/INotification.java
+++ tripmate/src/main/java/grp16/tripmate/notification/INotification.java
@@ -1,7 +1,6 @@
 package grp16.tripmate.notification;
 
 public interface INotification {
-    public void verification(String userEmail);
     public void requestNotification();
     public void responseNotification();
 }
