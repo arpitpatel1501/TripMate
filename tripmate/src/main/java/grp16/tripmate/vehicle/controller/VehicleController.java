53c60692220898f55c9ddd8c8b9df11071e93085
Ronil Patel <ronil.patel@dal.ca>
2022-12-10 18:40:44 -0400

    Added all the URL endpoints for the Vehicle module with Skeleton code

--- tripmate/src/main/java/grp16/tripmate/vehicle/controller/VehicleController.java
+++ tripmate/src/main/java/grp16/tripmate/vehicle/controller/VehicleController.java
@@ -2,16 +2,14 @@ package grp16.tripmate.vehicle.controller;
 
 import grp16.tripmate.logger.ILogger;
 import grp16.tripmate.logger.MyLoggerAdapter;
-import grp16.tripmate.post.model.IPost;
-import grp16.tripmate.post.model.IPostFactory;
-import grp16.tripmate.post.model.Post;
-import grp16.tripmate.post.model.PostFactory;
 import grp16.tripmate.vehicle.model.IVehicle;
 import grp16.tripmate.vehicle.model.IVehicleFactory;
 import grp16.tripmate.vehicle.model.Vehicle;
 import grp16.tripmate.vehicle.model.VehicleFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -28,11 +26,26 @@ public class VehicleController implements IVehicleController
         vehicle = vehicleFactory.getNewVehicle();
     }
 
-    @GetMapping("/vehicles")
+    @GetMapping("/all-vehicles")
     public String getAllVehicles(Model model) {
         model.addAttribute("title", "Vehicles");
         List<Vehicle> vehicles = vehicle.getAllVehicles();
         model.addAttribute("vehicles", vehicles);
         return "listvehicles";
+    }
+
+    @PostMapping("/vehicle/{id}")
+    public String confirmVehicleBooking(Model model, @PathVariable("id") int vehicleId) {
+        return "vehicledetails";
+    }
+
+    @GetMapping("/my-vehicle-bookings")
+    public String getAllVehicleBookingsByUserId(Model model) {
+        return "my_vehiclebookings";
+    }
+
+    @GetMapping("/recommended-vehicles")
+    public String getRecommendedVehiclesByTripId(Model model) {
+        return "recommended_vehicles";
     }
 }
