121568c1bd35fded21bcbd5399bb18c94a9f027c
amanjot <am350894@dal.ca>
2022-12-13 14:13:13 -0400

    Removing generic exception in Post Controller

--- tripmate/src/main/java/grp16/tripmate/post/model/IPost.java
+++ tripmate/src/main/java/grp16/tripmate/post/model/IPost.java
@@ -7,7 +7,6 @@ import grp16.tripmate.post.model.exception.MinAgeGreaterThanMaxAgeException;
 import grp16.tripmate.post.model.exception.StartDateAfterEndDateException;
 import grp16.tripmate.post.model.exception.StartDateBeforeTodayException;
 import grp16.tripmate.vehicle.database.IVehicleBookingDatabase;
-import grp16.tripmate.vehicle.model.IVehicleBookingFactory;
 import grp16.tripmate.vehicle.model.VehicleBooking;
 
 import java.text.ParseException;
@@ -17,7 +16,7 @@ public interface IPost {
 
     boolean createPost(IPostDatabase database) throws Exception;
 
-    List<Post> getAllPosts(IPostDatabase database, int loggedInUser) throws Exception;
+    List<Post> getAllPosts(IPostDatabase database, int loggedInUser);
 
     List<Post> getPostsByUserId(IPostDatabase database, int userid) throws Exception;
 
