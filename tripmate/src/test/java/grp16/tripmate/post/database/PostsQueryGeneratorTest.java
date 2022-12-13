21feccdf6e63e3f3254385fd2c77768ff6efaf9b
Harshil <hr830930@dal.ca>
2022-12-13 17:32:13 -0400

    View post refractor

---
+++ tripmate/src/test/java/grp16/tripmate/post/database/PostsQueryGeneratorTest.java
@@ -0,0 +1,40 @@
+package grp16.tripmate.post.database;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostsQueryGeneratorTest {
+
+    @Test
+    void getCreatePostQuery() {
+    }
+
+    @Test
+    void getAllPosts() {
+    }
+
+    @Test
+    void getPostsByUserId() {
+    }
+
+    @Test
+    void getPostByPostId() {
+    }
+
+    @Test
+    void getUpdatePostQuery() {
+    }
+
+    @Test
+    void deletePostQuery() {
+    }
+
+    @Test
+    void hidePostQuery() {
+    }
+
+    @Test
+    void getFeedbackPosts() {
+    }
+}
--- tripmate/src/test/java/grp16/tripmate/user/database/UserQueryGeneratorTest.java
+++ tripmate/src/test/java/grp16/tripmate/user/database/UserQueryGeneratorTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.security.NoSuchAlgorithmException;
 import java.text.ParseException;
 
-class UserQueryBuilderTest {
+class UserQueryGeneratorTest {
 
 
     IUserQueryGenerator queryBuilder = UserQueryGenerator.getInstance();
