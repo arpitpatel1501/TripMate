bba6776c4d20a269e1d1196ec639e33cb405fc4c
Harshil <hr830930@dal.ca>
2022-12-14 12:41:24 -0400

    more refractor

---
+++ tripmate/src/test/java/grp16/tripmate/myPostRequest/model/MyPostRequestTest.java
@@ -0,0 +1,132 @@
+package grp16.tripmate.myPostRequest.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyPostRequestTest {
+
+    @Test
+    void getIdRequest() {
+    }
+
+    @Test
+    void getFirstNameRequester() {
+    }
+
+    @Test
+    void getLastNameRequester() {
+    }
+
+    @Test
+    void getEmailRequester() {
+    }
+
+    @Test
+    void getPostTitle() {
+    }
+
+    @Test
+    void getIdCreator() {
+    }
+
+    @Test
+    void getIdRequester() {
+    }
+
+    @Test
+    void getEmailCreator() {
+    }
+
+    @Test
+    void getFirstNameCreator() {
+    }
+
+    @Test
+    void getLastNameCreator() {
+    }
+
+    @Test
+    void getStatus() {
+    }
+
+    @Test
+    void setStatus() {
+    }
+
+    @Test
+    void setIdRequest() {
+    }
+
+    @Test
+    void setFirstNameRequester() {
+    }
+
+    @Test
+    void setLastNameRequester() {
+    }
+
+    @Test
+    void setEmailRequester() {
+    }
+
+    @Test
+    void setPostTitle() {
+    }
+
+    @Test
+    void setIdCreator() {
+    }
+
+    @Test
+    void setIdRequester() {
+    }
+
+    @Test
+    void setFirstNameCreator() {
+    }
+
+    @Test
+    void setLastNameCreator() {
+    }
+
+    @Test
+    void setEmailCreator() {
+    }
+
+    @Test
+    void getMyPostRequests() {
+    }
+
+    @Test
+    void createJoinRequest() {
+    }
+
+    @Test
+    void getPostOwnerDetails() {
+    }
+
+    @Test
+    void updateRequest() {
+    }
+
+    @Test
+    void getPostRequesterDetails() {
+    }
+
+    @Test
+    void listToMyPostRequest() {
+    }
+
+    @Test
+    void listToMyPostRequestPostOwner() {
+    }
+
+    @Test
+    void listToPostRequesterDetails() {
+    }
+
+    @Test
+    void getMyRequestByUserId() {
+    }
+}
---
+++ tripmate/src/test/java/grp16/tripmate/post/model/factory/PostFactoryTest.java
@@ -0,0 +1,40 @@
+package grp16.tripmate.post.model.factory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostFactoryTest {
+
+    @Test
+    void makeNewPost() {
+    }
+
+    @Test
+    void makePostDatabase() {
+    }
+
+    @Test
+    void makePostQueryBuilder() {
+    }
+
+    @Test
+    void makeFeedbackDatabase() {
+    }
+
+    @Test
+    void makeFeedbackQueryBuilder() {
+    }
+
+    @Test
+    void makePostValidator() {
+    }
+
+    @Test
+    void makeNewLogger() {
+    }
+
+    @Test
+    void makeNewDatabaseExecutor() {
+    }
+}
